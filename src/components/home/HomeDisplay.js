b27fb08ca8805682758db8a6322e8e0cc49795da
MonseP <monse_plata_100@hotmail.com>
2018-01-25 15:01:24 -0600

    Cambios admin

--- src/components/home/HomeDisplay.js
+++ src/components/home/HomeDisplay.js
@@ -1,30 +1,70 @@
 import React, { Component } from 'react';
 import './Home.css';
 import {AboutDisplay} from './AboutDisplay';
-import {DestacadosDisplay} from './DestacadosDisplay';
+import Product from '../product/Product';
 import Nav from '../nav/Nav';
 import {PubliDisplay} from './PubliDisplay';
-import {InfoDisplay} from './InfoDisplay';
-import {RevistaDisplay} from './RevistaDisplay';
+import Contacto from '../../components/contacto/Contacto';
 import Footer from '../footer/Footer';
+import sr from './scrollReveal.js';
+
+import {SlideDisplay} from './SlideDisplay';
 
 class HomeDisplay extends Component {
+    componentDidMount () {
+        window.scroll(0, 0)
+        const config = {
+            origin: 'right',
+            duration: 1000,
+            delay: 150,
+            distance: '100%',
+            scale: 1,
+            easing: 'ease',
+        }
 
+        const config2 = {
+            origin: 'left',
+            duration: 1000,
+            delay: 150,
+            distance: '100%',
+            scale: 1,
+            easing: 'ease',
+        }
+        const config3 = {
+            origin: 'bottom',
+            duration: 800,
+            delay: 100,
+            distance: '100%',
+            scale: 1,
+            easing: 'ease',
+        }
+        const config4 = {
+            origin: 'top',
+            duration: 800,
+            delay: 100,
+            distance: '100%',
+            scale: 1,
+            easing: 'ease',
+        }
+
+        sr.reveal('.right', config2);
+        sr.reveal('.left', config);
+        sr.reveal('.bot', config3);
+        sr.reveal('.top', config4);
+
+
+
+
+    }
     render() {
     return (
             <div>
-                <div className="home_back">
-                    <div id='cubrir' className="cover ">
-                        <Nav />
-                        <h1>Línea órganica</h1>
-                        <p>Nueva colección de productos órganicos</p>
-                    </div>
-                </div>
+              <SlideDisplay />
+                <Nav />
                 <AboutDisplay />
-                <DestacadosDisplay />
+                <Product />
                 <PubliDisplay />
-                <InfoDisplay />
-                <RevistaDisplay />
+                <Contacto />
                 <Footer />
             </div>
         );
